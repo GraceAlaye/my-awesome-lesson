2b590322ef87555d33ecd3dd960997408e89bc9b
GraceAlaye <gracealaye@gmail.com>
2014-07-22 23:44:28 +0000

    update

---
+++ master/my-lesson.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+// TODO 1 : Create a varible called welcome message
+
+var welcomemessage = "Choose Your ____";
+console.log(welcomemessage);
+
+// TODO 2 : Create a varible called person
+
+var person = {};   
+console.log(person); 
+
+// TODO 3 : Add firsname and lastname properites to your player
+
+person.firstName = "  "; 
+person.lastName = " ";
+
+console.log("First Name: " + person.firstName); 
+console.log("Last Name: " + person.lastName);
+
+// TODO 4 : Add a status to your player using array syntax
+
+person["status"] = " ";
+console.log("status: " + person.status); 
+
+// TODO 5 : Create a function that prints the firstname lasntname and status of your player along with a quote 
+person.sayHello = function () { 
+  console.log("Hulk smash!");
+}
+person.sayHello();
