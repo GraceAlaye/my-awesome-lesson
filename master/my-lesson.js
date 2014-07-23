186d2fc10a60e782d6cb1d22fb7075501073d3b5
GraceAlaye <gracealaye@gmail.com>
2014-07-23 00:35:43 +0000

    update

--- master/my-lesson.js
+++ master/my-lesson.js
@@ -2,7 +2,7 @@
 
 // TODO 1 : Create a varible called welcome message
 
-var welcomemessage = "";
+var welcomemessage = "Marvel vs DC Universe ";
 console.log(welcomemessage);
 
 // TODO 2 : Create a varible called person
@@ -12,19 +12,19 @@ console.log(person);
 
 // TODO 3 : Add firsname and lastname properites to your player
 
-person.firstName = "  "; 
-person.lastName = " ";
+person.firstName = " The "; 
+person.lastName = "Hulk ";
 
 console.log("First Name: " + person.firstName); 
 console.log("Last Name: " + person.lastName);
 
 // TODO 4 : Add a status to your player using array syntax
 
-person["status"] = " ";
+person["status"] = "The Hulk ";
 console.log("status: " + person.status); 
 
 // TODO 5 : Create a function that prints a quote of the character 
 person.sayHello = function () { 
-  console.log("");
+  console.log("Hulk Smash");
 }
 person.sayHello();
